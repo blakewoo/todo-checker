fcd4412d5b7d5c3847b6d6caa406eaf1c590950a
blakewoo <blakewoo0819@gmail.com>
2023-02-05 20:35:31 +0900

    add calendar in main page

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,37 +1,53 @@
-function calendar(targetDiv,date) {
+function calendar(targetDiv,targetDate) {
     if(targetDiv===null || targetDiv===undefined){
         console.error("targetDiv is not provided")
         return;
     }
 
-    if(date===null || date===undefined){
-        date = new Date()
+    if(targetDate===null || targetDate===undefined){
+        targetDate = new Date()
     }
 
-    let thisDate = date
+    let thisDate = targetDate
     let thisMonthFirstDay = new Date(thisDate.getFullYear(), thisDate.getMonth(), 1);
     let thisMonthLastDay = new Date(thisDate.getFullYear(), thisDate.getMonth() + 1, 0);
 
     targetDiv.innerHTML = drawTable(thisDate.getFullYear(),thisDate.getMonth()+1)
 
     // 실제로 날짜 생성하는 로직
-
-
+    let tbody = document.getElementById("calendar_table_tbody")
+    let cnt = 0;
+    let str = "<tr>"
+    for(let i=0;i<thisMonthFirstDay.getDay();i++) {
+        str += "<td></td>"
+        cnt += 1
+    }
+    for(let i=1;i<thisMonthLastDay.getDate();i++) {
+        if(cnt%7===0) {
+            str += "<tr>"
+        }
+        cnt+=1
+        str += "<td class='day_td'><label class='day_label'>"+i+"</label></td>"
+        if(cnt%7===0) {
+            str += "</tr>"
+        }
+    }
+    tbody.innerHTML = str;
+    btnEvent()
 
     function drawTable (year,month) {
         var html = "<table class='calendar_table'>" +
             "<thead>" +
             "<tr class='title_arrow_tr'>"+
-            "<th><</th>" +
-            "<th colspan='5'>"+year+"년 "+month+"월</th>" +
-            "<th>></th>" +
+            "<td id='calendar_prevMonth'><</td>" +
+            "<td colspan='5'>"+year+"년 "+month+"월</td>" +
+            "<td id='calendar_nextMonth'>></td>" +
             "</tr>" +
-            "<thead>" +
             "<tr class='day_tr'>" +
             "<td>월</td><td>화</td><td>수</td><td>목</td><td>금</td><td>토</td><td>일</td>" +
             "</tr>" +
             "</thead>" +
-            "<tbody>" +
+            "<tbody id='calendar_table_tbody'>" +
             "</tbody>" +
             "</table>"
 
@@ -39,7 +55,31 @@ function calendar(targetDiv,date) {
     }
 
     function btnEvent() {
+        document.getElementById("calendar_prevMonth").removeEventListener("click",prevButtonEvent)
+        document.getElementById("calendar_nextMonth").removeEventListener("click",nextButtonEvent)
+        document.getElementById("calendar_prevMonth").addEventListener("click",prevButtonEvent)
+        document.getElementById("calendar_nextMonth").addEventListener("click",nextButtonEvent)
+        let days = document.getElementsByClassName("day_td")
+        for(let i=0;i<days.length;i++) {
+            days[i].removeEventListener("click",daySelectionEvent)
+            days[i].addEventListener("click",daySelectionEvent)
+        }
+    }
 
+    function prevButtonEvent(event){
+        let newDate = new Date(targetDate.getFullYear(),targetDate.getMonth()-1,targetDate.getDate())
+        calendar(targetDiv,newDate)
+    }
+    function nextButtonEvent(event) {
+        let newDate = new Date(targetDate.getFullYear(),targetDate.getMonth()+1,targetDate.getDate())
+        calendar(targetDiv,newDate)
+    }
+    function daySelectionEvent(event) {
+        let temp = document.getElementsByClassName("day_selected")
+        for(let i=0;i<temp.length;i++) {
+            temp[i].classList.remove("day_selected")
+        }
+        event.currentTarget.querySelector("label").classList.add("day_selected")
     }
 
 }
