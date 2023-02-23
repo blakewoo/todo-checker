cd5fdc41da95b2f3860ee876957c91a91eb9f799
blakewoo <blakewoo0819@gmail.com>
2023-02-23 23:56:08 +0900

    Add comment insertion function

--- routes/r_commentlist.js
+++ routes/r_commentlist.js
@@ -1,11 +1,13 @@
 var express = require('express');
 let verify = require("../module/m_verify_user")
+const commentModule = require("../module/m_comment")
 var router = express.Router();
 
 // TODO 불러들이기
 router.get('/my',verify.user_auth,function(req, res, next) {
     try{
         // get my comment list
+
     }
     catch(e){
         return res.status(404).send({status:false})
@@ -14,7 +16,26 @@ router.get('/my',verify.user_auth,function(req, res, next) {
 
 // TODO 추가
 router.post('/my',verify.user_auth,async function(req, res, next) {
-
+    try{
+        // get my comment list
+        if(!req.body.USER_ID) {
+            return res.status(400).send({status:false})
+        }
+        if(!req.body.TARGET_DATE) {
+            return res.status(400).send({status:false})
+        }
+        if(!req.body.PARENT) {
+            return res.status(400).send({status:false})
+        }
+        if(!req.body.DATA) {
+            return res.status(400).send({status:false})
+        }
+        await commentModule.addComment(req.body.USER_ID,req.body.TARGET_DATE,req.body.PARENT,req.body.DATA)
+        return res.send({status:true})
+    }
+    catch(e){
+        return res.status(404).send({status:false})
+    }
 });
 
 // TODO 업데이트
