0c9614dae1fbf17168a6f2ead5c56b0b5f76cf4c
blakewoo <blakewoo0819@gmail.com>
2023-03-31 20:23:13 +0900

    Creating a schedule sharing request page(not complete yet)

--- public/javascripts/j_schedule_setting.js
+++ public/javascripts/j_schedule_setting.js
@@ -1,7 +1,32 @@
 window.onload = function(){
     headlineInit()
+    initSchedule()
 
     function initSchedule() {
-        
+        let requestList = sharedRequestStatus()
+        printSharedRequestStatus(requestList)
+
+        let receiveList = sharedRequestReceiveStatus()
+        printSharedRequestReceiveStatus(receiveList)
+    }
+
+    function sharedRequestStatus() {
+        requestFunction("GET","/",{},"JSON",function (result){
+
+        })
+    }
+
+    function sharedRequestReceiveStatus() {
+        requestFunction("GET","/",{},"JSON",function (result){
+
+        })
+    }
+
+    function printSharedRequestStatus(list){
+
+    }
+
+    function printSharedRequestReceiveStatus(list){
+
     }
 }
