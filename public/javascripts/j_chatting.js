5febef570922f93f3ed1a2b50618db45e3f12a45
blakewoo <blakewoo0819@gmail.com>
2023-04-11 22:10:41 +0900

    Working on adding chat feature

--- public/javascripts/j_chatting.js
+++ public/javascripts/j_chatting.js
@@ -1,5 +1,23 @@
-window.onload = async function (event) {
+window.onload = function (event) {
     headlineInit()
     // Chatting with your viewer
+    document.getElementById("messageSendButton").addEventListener("click",sendMessage)
+    document.getElementById("chattingTextarea").addEventListener("keyup",enterSendMessage)
 
+    function enterSendMessage(event) {
+        if(event.key==="Enter") {
+            document.getElementById("messageSendButton").click()
+        }
+    }
+
+    function sendMessage(event) {
+        let text = document.getElementById("chattingTextarea").value
+        //backend update
+
+        //socket connect
+
+        //front update
+        document.getElementById("textDisplayDiv").innerText += "나 : "+text + "\n"
+        document.getElementById("chattingTextarea").value = ""
+    }
 }
