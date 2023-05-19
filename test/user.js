46a5c5fe4f2f1242a11f0b8143e163f151b50e1d
blakewoo <blakewoo0819@gmail.com>
2023-05-19 23:23:29 +0900

    Update user module test code (need modification)

--- test/user.js
+++ test/user.js
@@ -2,5 +2,27 @@ const userModule = require('../module/m_user');
 const assert = require('assert');
 
 describe('USER CRUD TEST', function () {
+    const userTestInputData = []
+    const userTestAddResultData = []
+    const userTestReadResultData = []
 
+    const userTestUpdateInputData = []
+    const userTestUpdateResultData = []
+    const userTestDeleteResultData = []
+
+    let count = 0;
+
+    beforeEach(function () {
+        assert.equal(userModule.addUser(userTestInputData[count][0],userTestInputData[count][0],userTestInputData[count][0]), userTestAddResultData[count]);
+    });
+    afterEach(function () {
+        assert.equal(userModule.deleteUser(userTestInputData[count][0]), userTestDeleteResultData[count]);
+        count += 1
+    });
+    it('READ USER', function () {
+        assert.equal(userModule.getUser(userTestInputData[count][0]), userTestReadResultData[count]);
+    });
+    it('UPDATE USER', function () {
+        assert.equal(userModule.updateUser(userTestInputData[count][0],userTestUpdateInputData[count][0],userTestUpdateInputData[count][1]).status, userTestUpdateResultData[count]);
+    });
 });
