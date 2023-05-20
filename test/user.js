3d4d95bc036f339639e2bd6010e3651a93e47986
blakewoo <blakewoo0819@gmail.com>
2023-05-20 23:09:08 +0900

    Update test code & Update test command

--- test/user.js
+++ test/user.js
@@ -2,7 +2,7 @@ const userModule = require('../module/m_user');
 const assert = require('assert');
 
 describe('USER CRUD TEST', function () {
-    const userTestInputData = []
+    const userTestInputData = [2,2,2,2,2]
     const userTestAddResultData = []
     const userTestReadResultData = []
 
@@ -11,18 +11,31 @@ describe('USER CRUD TEST', function () {
     const userTestDeleteResultData = []
 
     let count = 0;
+    let dataLength = userTestInputData.length
 
-    beforeEach(function () {
-        assert.equal(userModule.addUser(userTestInputData[count][0],userTestInputData[count][0],userTestInputData[count][0]), userTestAddResultData[count]);
+    before(async () => {
+       // DB connection 변경
     });
-    afterEach(function () {
-        assert.equal(userModule.deleteUser(userTestInputData[count][0]), userTestDeleteResultData[count]);
-        count += 1
+
+    after(async () => {
+        // DB connection 변경
     });
-    it('READ USER', function () {
-        assert.equal(userModule.getUser(userTestInputData[count][0]), userTestReadResultData[count]);
-    });
-    it('UPDATE USER', function () {
-        assert.equal(userModule.updateUser(userTestInputData[count][0],userTestUpdateInputData[count][0],userTestUpdateInputData[count][1]).status, userTestUpdateResultData[count]);
-    });
+
+    for(;count<dataLength;count++) {
+        describe((count+1)+'번째 테스트 케이스', function () {
+            it('CREATE USER', function () {
+                // assert.equal(userModule.addUser(userTestInputData[count][0],userTestInputData[count][0],userTestInputData[count][0]), userTestAddResultData[count]);
+            });
+            it('READ USER', function () {
+                // assert.equal(userModule.getUser(userTestInputData[count][0]), userTestReadResultData[count]);
+            });
+            it('UPDATE USER', function () {
+                // assert.equal(userModule.updateUser(userTestInputData[count][0],userTestUpdateInputData[count][0],userTestUpdateInputData[count][1]).status, userTestUpdateResultData[count]);
+            });
+            it('DELETE USER', function () {
+                // assert.equal(userModule.deleteUser(userTestInputData[count][0]), userTestDeleteResultData[count]);
+            });
+        })
+    }
+
 });
