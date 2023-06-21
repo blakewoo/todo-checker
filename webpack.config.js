e32fa906f770ffd7b53724fed79d0d9f3b614238
blakewoo <blakewoo0819@gmail.com>
2023-06-21 22:46:33 +0900

    Researching webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,35 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./public/javascripts/main.js",
+    entry: {
+        all:"./public/javascripts/*.js",
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename : '[name]_bundle.js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                use: "babel-loader",
                 exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.ejs$/,
+                loader: 'ejs-loader',
+                options: {
+                    variable: 'data',
+                    interpolate : '\\{\\{(.+?)\\}\\}',
+                    evaluate : '\\[\\[(.+?)\\]\\]'
+                }
             }
         ],
     },
