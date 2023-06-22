ab6fbb8b856087e175ea3da6a1106c970db0250a
blakewoo <blakewoo0819@gmail.com>
2023-06-22 22:33:51 +0900

    Separation of development and deployment configuration files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,11 +3,22 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: {
-        all:"./public/javascripts/*.js",
+        j_calendar:"./public/javascripts/j_calendar.js",
+        j_chatting:"./public/javascripts/j_chatting.js",
+        j_datepicker:"./public/javascripts/j_datepicker.js",
+        j_headline:"./public/javascripts/j_headline.js",
+        j_login:"./public/javascripts/j_login.js",
+        j_myInfo:"./public/javascripts/j_myInfo.js",
+        j_mySchedule:"./public/javascripts/j_mySchedule.js",
+        j_publicFunction:"./public/javascripts/j_publicFunction.js",
+        j_signUp:"./public/javascripts/j_signUp.js",
+        j_targetSchedule:"./public/javascripts/j_targetSchedule.js",
+        j_todo:"./public/javascripts/j_todo.js",
+        j_underConstruction:"./public/javascripts/j_underConstruction.js"
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename : '[name]_bundle.js'
+        path: path.resolve("/public/dist"),
+        filename : '[name].js'
     },
     module: {
         rules: [
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: {
+        j_calendar:"./public/javascripts/j_calendar.js",
+        j_chatting:"./public/javascripts/j_chatting.js",
+        j_datepicker:"./public/javascripts/j_datepicker.js",
+        j_headline:"./public/javascripts/j_headline.js",
+        j_login:"./public/javascripts/j_login.js",
+        j_myInfo:"./public/javascripts/j_myInfo.js",
+        j_mySchedule:"./public/javascripts/j_mySchedule.js",
+        j_publicFunction:"./public/javascripts/j_publicFunction.js",
+        j_signUp:"./public/javascripts/j_signUp.js",
+        j_targetSchedule:"./public/javascripts/j_targetSchedule.js",
+        j_todo:"./public/javascripts/j_todo.js",
+        j_underConstruction:"./public/javascripts/j_underConstruction.js"
+    },
+    output: {
+        path: "/dist",
+        filename : '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.ejs$/,
+                loader: 'ejs-loader',
+                options: {
+                    variable: 'data',
+                    interpolate : '\\{\\{(.+?)\\}\\}',
+                    evaluate : '\\[\\[(.+?)\\]\\]'
+                }
+            }
+        ],
+    },
+    plugins: [],
+};
