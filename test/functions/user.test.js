c28b9405ffbf47478b658284eb7e2aca3a88e8b0
blakewoo <blakewoo0819@gmail.com>
2023-07-24 22:21:54 +0900

    Add single function test code

---
+++ test/functions/user.test.js
@@ -0,0 +1,18 @@
+
+
+//CRUD user test
+describe("GET /user/my", () => {
+
+})
+
+describe("POST /user/my", () => {
+
+})
+
+describe("PUT /user/my", () => {
+
+})
+
+describe("DELETE /user/my", () => {
+
+})
