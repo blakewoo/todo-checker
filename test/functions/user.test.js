eabc59fe14d10ab835276bc6b7506e72af1262b3
blakewoo <blakewoo0819@gmail.com>
2023-07-25 22:04:41 +0900

    Update user CRUD test

--- test/functions/user.test.js
+++ test/functions/user.test.js
@@ -1,18 +1,56 @@
+const request = require("supertest")
+const makeApp = require("../../app")
+let getUserFromId = jest.fn()
 
 
-//CRUD user test
-describe("GET /user/my", () => {
+let mariaDB = {
+    user:{
+        getUserFromId:getUserFromId
+    }
+}
+let mongoDB = {}
+const app = makeApp(mariaDB,mongoDB)
 
+beforeAll(()=>{
+    getUserFromId.mockResolvedValue([{ID:"testman",PASSWORD:"dfdfdf",EMAIL:"dfdfdfdfd@naver.com"}])
 })
 
-describe("POST /user/my", () => {
+describe("Test case when not authorized",()=>{
+    //CRUD user test
+    test("GET /user/my", async () => {
+        let response = await request(app).get("/user/my")
+        expect(response.status).toBe(401)
+    })
 
+    test("POST /user/my", () => {
+
+    })
+
+    test("PUT /user/my", () => {
+
+    })
+
+    test("DELETE /user/my", () => {
+
+    })
 })
 
-describe("PUT /user/my", () => {
 
-})
+describe("Test case when authorized",()=>{
+    //CRUD user test
+    test("GET /user/my", async () => {
 
-describe("DELETE /user/my", () => {
+    })
 
+    test("POST /user/my", () => {
+
+    })
+
+    test("PUT /user/my", () => {
+
+    })
+
+    test("DELETE /user/my", () => {
+
+    })
 })
